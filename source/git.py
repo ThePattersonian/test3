b7603a831ab81fa3b63ad990c9167d99fffd25db
Ian Patterson <ianpatterson@ianpattrsonsmbp.gateway.sonic.net>
2020-07-13 10:47:39 -0700

    Hope this saves

--- source/git.py
+++ source/git.py
@@ -3,16 +3,16 @@
  Working with Git
 ==================
 
-  As this is my first project working with git, github and/or gitlab, it's also
+As this is my first project working with git, github and/or gitlab, it's also
 my first project working with any sort of version control system. On this page,
 I'll be writing about the steps used with git to make things work.
 
-  The first thing to do with git is to turn a directory (a folder) into a
+The first thing to do with git is to turn a directory (a folder) into a
 repository using the Command Line Interface (CLI) command::
 
   git init
 
-  Then I need to add some content to the folder. To update the repository, I need
+Then I need to add some content to the folder. To update the repository, I need
 to use some version of ``git add``. Files can be added one by one, or they can
 be added all at once or just the updated ones using the following commands::
 
