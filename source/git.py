dbf2352c543caf1667ef5568ba3b5cf51c197fb9
Ian Patterson <ianpatterson@ianpattrsonsmbp.gateway.sonic.net>
2020-07-13 11:33:31 -0700

    Changed makefile

---
+++ source/git.py
@@ -0,0 +1,27 @@
+"""
+==================
+ Working with Git
+==================
+
+As this is my first project working with git, github and/or gitlab, it's also
+my first project working with any sort of version control system. On this page,
+I'll be writing about the steps used with git to make things work.
+
+The first thing to do with git is to turn a directory (a folder) into a
+repository using the Command Line Interface (CLI) command::
+
+  git init
+
+Then I need to add some content to the folder. To update the repository, I need
+to use some version of ``git add``. Files can be added one by one, or they can
+be added all at once or just the updated ones using the following commands::
+
+  git add <filename>
+  git add .
+  git add -A
+  git add -u
+
+There
+"""
+def my_function():
+    pass
