d7cbc1e6448888fbb2e0c93f9e11ef089d5cd1f8
Ian Patterson <ianpatterson@ianpattrsonsmbp.gateway.sonic.net>
2020-07-22 10:44:37 -0700

    Added github vs gitlab

---
+++ source/githublab.py
@@ -0,0 +1,40 @@
+"""
+==================
+ Github vs Gitlab
+==================
+
+Here I'll be documenting my experience with using Github and Gitlab, comparing
+the two and seeing which one works better for the purposes of making this blog.
+
+
+Github
+=======
+
+One of the major benefits of using Github is that there are millions of
+repositories that use it and it has become one of the standards for Version
+Control Systems (VCS). As such, learning to navigate it and work within its
+ecosystem will be highly beneficial for me. That being said, I find that
+Github's own documentation is lacking and quite often I'll need to look up how
+to do something over on StackOverflow.
+
+One major problem, at least for me and my attempt at creating something akin to
+a blog with Github, is that in order to host a page for a project on Github, the
+project must be public. Additionally, Github limits the number of projects one
+can have for free.
+
+Gitlab
+========
+
+Gitlab is a smaller VCS ecosystem than Github currently, but it seems to be
+picking up marketshare. This is probably due to its focus on creating whole
+pipelines (whatever that jargon word is supposed to describe) and its focus on
+continuous delivery (CI/CD). Additionally, Gitlab has more documentation built
+in to its own ecosystem, such that in order to get a page up and running on
+Gitlab.io, I was able to find most of the relevant steps on Gitlab hosted pages.
+Gitlab also makes it easy to import projects from other VCSs and allows people
+to create pages for private projects.
+
+In short, I think Gitlab offers many extra features and is easier to use than
+Github. However, I think I will still be using Github for this project as its
+more of a learning experience.
+"""
